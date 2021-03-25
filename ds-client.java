993b2fe54e8b0d90b9d5f879a7fc6c61d72cf3e1
nathshaheen <nathan.shaheen@students.mq.edu.au>
2021-03-25 15:30:50 +1100

    Seperated handshake into a seperate function

--- ds-client.java
+++ ds-client.java
@@ -5,37 +5,57 @@
  *
  */
 
-import java.io.*;
+import java.io.*;  
 import java.net.*;
 
-public class TCPtest {
+public class DSClient {  
     public static void main(String[] args) {
         Socket s = null;
 
         try {
             // Open a port to the server
-            int serverPort = 50000; // TODO: What is the port number?
-            s = new Socket("localhost", serverPort);
-            DataInputStream dis = new DataInputStream(s.getInputStream());
-            DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+            int serverPort = 50000;  // TODO: What is the port number?      
+            s = new Socket("localhost", serverPort);  
+            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            DataOutputStream out = new DataOutputStream(s.getOutputStream());
 
             // Setup a connection following the defined protocol
-            dout.write(("HELO\n").getBytes());
+            handshake(in, out);
+            
+            // Read XML
 
-            String str = (String) dis.readLine();
+            if (((String)in.readLine()).equals("OK")) {
+                out.write(("REDY\n").getBytes());
 
-            if (str.equals("OK"))
-                dout.write(("AUTH franciscoB\n").getBytes());
+                while (!((String)in.readLine()).equals("NONE")) {
+                    // Parse incomming String (Core count, RAM, Disk Space)
 
-            while (str.equals("REDY")) {
-                // Handle job
+                    // Create String ("SCH...") and send to DS-SERVER
+
+                    // Send "REDY" to get next job
+                }
             }
-            dout.write(("QUIT\n").getBytes());
-            s.close(); // Close the connection
-        }
 
+            out.write(("QUIT\n").getBytes());
+
+            if (((String)in.readLine()).equals("QUIT"))
+                s.close();  // Close the connection 
+        }
+        
         catch (Exception e) {
             System.out.println(e);
-        }
+        }  
     }
-}
+
+    // Performs the initial handshake
+    public static void handshake(BufferedReader dis, DataOutputStream dout) {
+        try {
+            dout.write(("HELO\n").getBytes());
+
+            String str = dis.readLine();
+            if (str.equals("OK"))
+                dout.write(("AUTH Nathan\n").getBytes());
+        }
+        
+        catch (Exception e) {
+            System.out.println(e);
